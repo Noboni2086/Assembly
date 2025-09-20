125bd6c33bcba207cbfb6e3a2a618cce0313bf60
Shraboni Biswas Naboni <121560010+Noboni2086@users.noreply.github.com>
2025-09-20 19:18:20 +0600

    Add files via upload

---
+++ reverse using push pop.asm
@@ -0,0 +1,43 @@
+.model small
+.stack 100h
+.data
+.code
+main proc 
+    
+    xor cx,cx  
+    
+    
+    mov ah,1
+    int 21h
+    
+    while:
+    cmp al,0dh
+    je end_while
+    
+    push ax
+    inc cx
+    int 21h
+    
+    jmp while
+    
+    end_while:
+    
+    mov ah,2
+    mov dl,0dh
+    int 21h  
+    mov dl,0ah
+    int 21h
+    
+   ; jcxz exit
+    
+    top:
+    mov ah,2
+    pop dx
+    int 21h
+    loop top
+    
+    exit:
+    mov ah,4ch
+    int 21h
+    main endp
+end main 
